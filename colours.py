4bb527392c52a079318ef0696a7cd323af36aad1
michael-carlos-codes <mcarlos5@myseneca.ca>
2025-02-07 10:38:23 -0500

    Added colours.py script demonstrating symmetric difference

---
+++ colours.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This script demonstrates the use of symmetric difference on two sets.
+It calculates the symmetric difference between two sets and prints the result.
+"""
+
+# Creating two sets of colors
+x = {1, 1, 2, 5, 7}
+y = {1, 3, 4, 5}
+
+# Calculating the symmetric difference
+print(x ^ y)
